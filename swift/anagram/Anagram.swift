a32e0eeff68bccf0073d33fba6d513bdaa7a4d96
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2015-03-06 16:27:33 +0100

    Cleaning up my anagram solution to use the sorted() method rather than building a sort function

--- swift/anagram/Anagram.swift
+++ swift/anagram/Anagram.swift
@@ -1,13 +1,3 @@
-//
-//  Anagram.swift
-//  exercism-test-runner
-//
-//  Created by Kevin VanderLugt on 3/6/15.
-//  Copyright (c) 2015 Alpine Pipeline, LLC. All rights reserved.
-//
-
-import Foundation
-
 class Anagram {
     var word: String
     
@@ -16,17 +6,10 @@ class Anagram {
     }
 
     func match(words: [String]) -> [String] {
-        var matches = [String]()
-        let sortedMatchWord = sortedWord(word.lowercaseString)
-        
-        matches = words.filter { $0.lowercaseString != self.word.lowercaseString &&
-                                self.sortedWord($0.lowercaseString) == sortedMatchWord }
-        
-        return matches
-    }
-    
-    func sortedWord(word: String) -> String {
-        let wordArray = Array(word)
-        return String(wordArray.sorted { $0 < $1 })
+        let lowercaseWord = word.lowercaseString
+        return words.filter {
+            let anagram = $0.lowercaseString
+            return anagram != lowercaseWord && (sorted(anagram) == sorted(lowercaseWord))
+        }
     }
 }
