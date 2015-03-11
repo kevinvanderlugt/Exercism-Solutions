5322b79398b139b3364c5f4cd221008f1e95137d
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2015-03-11 19:20:08 +0100

    first complete pass at gradeschool problem

---
+++ swift/exercism-test-runner/exercism-test-runner/GradeSchool.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+class GradeSchool {
+    var db = [Int: [String]]()
+    
+    func addStudent(name: String, grade: Int) {
+        var students = studentsInGrade(grade)
+        students.append(name)
+        db[grade] = students
+    }
+    
+    func studentsInGrade(grade: Int) -> [String] {
+        return db[grade] ?? []
+    }g
+    
+    func sortedRoster() -> [Int: [String]] {
+        var sortedDb = db
+        for (grade, students) in sortedDb {
+            sortedDb[grade] = students.sorted { $0 < $1 }
+        }
+        return sortedDb
+    }
+}
