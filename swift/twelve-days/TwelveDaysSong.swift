1a71474e52792f75858759ff392d75e0612a2013
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2015-03-18 12:59:04 +0100

    Adding the Twelve days solution, I would have used an optional for verse() but the test cases didnt allow for that

---
+++ swift/twelve-days/TwelveDaysSong.swift
@@ -0,0 +1,38 @@
+//
+//  TwelveDaysSong.swift
+//  exercism-test-runner
+//
+//  Created by Kevin VanderLugt on 3/18/15.
+//  Copyright (c) 2015 Alpine Pipeline, LLC. All rights reserved.
+//
+
+import Foundation
+
+struct TwelveDaysSong {
+    static private let verses = [
+        1: "On the first day of Christmas my true love gave to me, a Partridge in a Pear Tree.\n",
+        2: "On the second day of Christmas my true love gave to me, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        3: "On the third day of Christmas my true love gave to me, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        4: "On the fourth day of Christmas my true love gave to me, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        5: "On the fifth day of Christmas my true love gave to me, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        6: "On the sixth day of Christmas my true love gave to me, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        7: "On the seventh day of Christmas my true love gave to me, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        8: "On the eighth day of Christmas my true love gave to me, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        9: "On the ninth day of Christmas my true love gave to me, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        10: "On the tenth day of Christmas my true love gave to me, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        11: "On the eleventh day of Christmas my true love gave to me, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n",
+        12: "On the twelfth day of Christmas my true love gave to me, twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n"
+    ]
+    
+    static func verse(number: Int) -> String {
+        return verses[number]!
+    }
+    
+    static func verses(start: Int, _ end: Int) -> String {
+        return reduce(start...end, "", { $0 + "\(self.verse($1))\n" })
+    }
+    
+    static func sing() -> String {
+        return verses(1, 12)
+    }
+}
