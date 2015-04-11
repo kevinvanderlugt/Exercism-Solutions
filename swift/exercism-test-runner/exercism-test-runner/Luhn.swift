0cc83f0937dd65d9df64ceb6f493bf7c487b5dde
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2015-04-11 19:51:49 +0200

    Modifying the Luhn checksumto include a base number

--- swift/exercism-test-runner/exercism-test-runner/Luhn.swift
+++ swift/exercism-test-runner/exercism-test-runner/Luhn.swift
@@ -5,8 +5,9 @@
 //  Created by Kevin VanderLugt on 4/11/15.
 //  Copyright (c) 2015 Alpine Pipeline, LLC. All rights reserved.
 //
-
 struct Luhn {
+    static let base = 10
+    
     var number: Int
     init(_ number: Int) {
         self.number = number
@@ -24,7 +25,7 @@ struct Luhn {
     var addends: [Int] {
         return reverse(map(enumerate(reversedComponents)) { (index, number) in
             var doubled = number * 2
-            doubled = doubled < 10 ? doubled : doubled - 9
+            doubled = doubled < Luhn.base ? doubled : doubled - (Luhn.base-1)
             return index % 2 == 0 ? number : doubled
         })
     }
@@ -34,16 +35,12 @@ struct Luhn {
     }
     
     var isValid: Bool {
-        return checksum % 10 == 0
+        return checksum % Luhn.base == 0
     }
     
     static func create(number: Int) -> Int {
-        var luhn = self(number)
-        var checkDigit = 0
-        while !luhn.isValid {
-            luhn.number = "\(number)\(checkDigit)".toInt()!
-            checkDigit++
-        }
-        return luhn.number
+        let luhn = self(number * base)
+        let checkDigit = (base - luhn.checksum % base) % base
+        return luhn.number + checkDigit
     }
 }
